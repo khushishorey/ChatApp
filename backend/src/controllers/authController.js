e5d7d654d6bfb7db5cbe96a0fec3a03eebf0abf7
khushi <khushishorey2005@gmail.com>
2025-12-25 01:35:55 +0530

    Made the login route and tested the login api using postman

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,12 +1,13 @@
 import User from "../models/User.js"
 import bcrypt from "bcryptjs"
+import { generateToken } from "../utils/generateToken.js"
 
 export const signup = async (req,res) => {
     const { name, email, password} = req.body
 
     try {
         if(!name || !email || !password)
-            return res.status(400).json({message: "Please fill all required fields"})
+            return res.status(400).json({message: "Please fill in all the required fields"})
         if(password.length < 6)
             return res.status(400).json({message: "Password must be atleast 6 characters long"})
 
@@ -34,7 +35,34 @@ export const signup = async (req,res) => {
 }
 
 export const login = async (req,res) => {
-    res.send("Signup Page")
+    const { email, password} = req.body
+
+    try {
+        const user = await User.findOne({email})
+
+        if(!user)
+            return res.status(400).json({message: "Invalid Credentials"})
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password)
+
+        if(!isPasswordCorrect)
+            return res.status(400).json({message: "Invalid Credentials"})
+
+        generateToken(user._id, res)
+
+        res.status(201).json(
+            {message: "Login successfull!!",
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            profilePic: user.profilePic,
+            })
+
+    } catch (error) {
+    console.error("LOGIN ERROR 👉", error);
+    return res.status(500).json({ message: error.message });
+    }
+
 }
 
 export const logout = async (req,res) => {
