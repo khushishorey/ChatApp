b43d870ea9c4b6b64446eae503456665202e8553
khushi <khushishorey2005@gmail.com>
2025-12-24 23:40:14 +0530

    Backend folders created and written user schema

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,11 @@
+export const signup = async (req,res) => {
+    res.send("Signup Page")
+}
+
+export const login = async (req,res) => {
+    res.send("Signup Page")
+}
+
+export const logout = async (req,res) => {
+    res.send("Signup Page")
+}
---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+import express from "express"
+import dotenv from "dotenv"
+import connectDB from "./config/db.js";
+import authRoute from "./routes/authRoute.js";
+
+dotenv.config()
+connectDB()
+
+const app = express()
+app.use("api/auth", authRoute)
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () =>{
+     console.log(`🚀 Server running on port ${PORT}`);
+})
